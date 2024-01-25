63b4e4addc118a1e1de2c3e65f33a7a62ced633c
Pelepo <andretta.filippo.99@gmail.com>
2024-01-25 12:39:01 +0100

    Prova new version app

--- components/NavBar/SideBar/SideBar.jsx
+++ components/NavBar/SideBar/SideBar.jsx
@@ -7,42 +7,10 @@ import Image from 'next/image';
 //INRTERNAL IMPORTS
 import Style from "./SideBar.module.css";
 import img from "./../../../img/index";
-import UpdateUsername from "../AccountSettings/UpdateUsername/UpdateUsername";
-import UpdatePassword from "../AccountSettings/UpdatePassword/UpdatePassword";
-import UnlinkWallet from "../AccountSettings/UnlinkCryptoWallet/UnlinkCryptoWallet";
-import DeleteAccount from "../AccountSettings/DeleteAccount/DeleteAccount";
 
+const SideBar = ({ setOpenArtistSettings, setOpenSideBar, user, setOpenRegister, setOpenLogin }) => {
 
-
-
-const SideBar = ({ setOpenSideBar, user, setOpenRegister, setOpenLogin }) => {
-
-    const { disconnectUser } = useContext(NFTMarketplaceContext);
-    const [update, setUpdate] = useState(false);
-    const [updateAccount, setUpdateAccount] = useState(false);
-    const [updatePassword, setUpdatePassword] = useState(false);
-    const [unlinkCryptoWallet, setUnlinkCryptoWallet] = useState(false);
-    const [deleteAccount, setDeleteAccount] = useState(false);
-
-    const openAndCloseUpdateAccount = () => {
-        setUpdateAccount(!updateAccount);
-        setUpdatePassword(false); setUnlinkCryptoWallet(false); setDeleteAccount(false);
-    }
-
-    const openAndCloseUpdatePassword = () => {
-        setUpdatePassword(!updatePassword);
-        setUpdateAccount(false); setUnlinkCryptoWallet(false); setDeleteAccount(false);
-    }
-
-    const openAndCloseUnlinkCryptoWallet = () => {
-        setUnlinkCryptoWallet(!unlinkCryptoWallet);
-        setUpdatePassword(false); setUpdateAccount(false); setDeleteAccount(false);
-    }
-
-    const openAndCloseDeleteAccount = () => {
-        setDeleteAccount(!deleteAccount);
-        setUpdatePassword(false); setUnlinkCryptoWallet(false); setUpdateAccount(false);
-    }
+    const { setOpenAccountSetting, disconnectUser } = useContext(NFTMarketplaceContext);
 
     return (
         <div className={Style.sidebar}>
@@ -51,65 +19,13 @@ const SideBar = ({ setOpenSideBar, user, setOpenRegister, setOpenLogin }) => {
                     <div className={Style.sidebar_center_userIn}>
                         <div className={Style.sidebar_center_userIn_top}>
                             <Link className={Style.sidebar_center_userIn_top_left} onClick={() => setOpenSideBar(false)} href={{ pathname: "/my-profile" }}>
-                                <Image src={img.user} alt="user icon" width={32} heigh={32} />
+                                <Image src={img[`utente_${user.picture}`]} alt="user icon" width={32} heigh={32} />
                                 <div className={`${Style.sidebar_center_userIn_top_email}`}>{user.displayName}</div>
                             </Link>
-                            <div onClick={() => setUpdate(!update)}>
+                            <div onClick={() => setOpenAccountSetting(true)}>
                                 <Image src={img.settings} alt="setting icon" width={32} heigh={32} />
                             </div>
                         </div>
-                        {update && (
-                            <div className={Style.sidebar_center_userIn_update}>
-                                <div className={Style.sidebar_center_userIn_update_box}>
-                                    <div className={`${Style.sidebar_center_userIn_update_box_title} font-medium`} onClick={() => openAndCloseUpdateAccount()}>
-                                        update username
-                                    </div>
-                                    {updateAccount &&
-                                        <div className={Style.sidebar_center_userIn_update_box_content}>
-                                            <UpdateUsername />
-                                        </div>
-                                    }
-                                </div>
-                                <div className={Style.sidebar_center_userIn_update_box}>
-                                    <div>
-                                        <div className={`${Style.sidebar_center_userIn_update_box_title} font-medium`} onClick={() => openAndCloseUpdatePassword()}>
-                                            update password
-                                        </div>
-                                        {updatePassword &&
-                                            <div className={Style.sidebar_center_userIn_update_box_content}>
-                                                <UpdatePassword />
-                                            </div>
-
-                                        }
-                                    </div>
-                                </div>
-                                {user.wallet && <div className={Style.sidebar_center_userIn_update_box}>
-                                    <div className={`${Style.sidebar_center_userIn_update_box_title} font-medium`} onClick={() => openAndCloseUnlinkCryptoWallet()}>
-                                        unlink crypto wallet
-                                    </div>
-                                    {unlinkCryptoWallet &&
-                                        <div className={Style.sidebar_center_userIn_update_box_content}>
-                                            <UnlinkWallet />
-                                        </div>
-                                    }
-                                </div>}
-                                <div className={Style.sidebar_center_userIn_update_box}>
-                                    <div className={`${Style.sidebar_center_userIn_update_box_title} font-medium`} onClick={() => openAndCloseDeleteAccount()}>
-                                        delete account
-                                    </div>
-                                    {deleteAccount &&
-                                        <div className={Style.sidebar_center_userIn_update_box_content} onMouseDown={(e) => e.stopPropagation()}>
-                                            <DeleteAccount />
-                                        </div>
-                                    }
-                                </div>
-                                <div className={Style.sidebar_center_userIn_update_box}>
-                                    <div className={`${Style.sidebar_center_userIn_update_box_title} font-medium`} onClick={() => disconnectUser()}>
-                                        log out
-                                    </div>
-                                </div>
-                            </div>
-                        )}
                     </div>
                 ) : (
                     <div className={Style.sidebar_center_login}>
@@ -120,15 +36,14 @@ const SideBar = ({ setOpenSideBar, user, setOpenRegister, setOpenLogin }) => {
                 <div className={Style.sidebar_center_element}>
                     <Link href={"./collection"} onClick={() => setOpenSideBar(false)}>collection</Link>
                 </div>
-                {user && <div className={Style.sidebar_center_element} >
-                    <Link href={"./my-profile"} onClick={() => setOpenSideBar(false)}>my items</Link>
-                </div>}
                 <div className={Style.sidebar_center_element} >
                     <Link href={"./about-us"} onClick={() => setOpenSideBar(false)}>about</Link>
                 </div>
-                <div className={Style.sidebar_center_bottom}>
-                    get in touch <a style={{ color: "var(--main-color)", textDecoration: "underline", textAlign: "right" }} href="mailto:info@lirmusic.com">info@lirmusic.com</a>
-                </div>
+                {user && <div className={Style.sidebar_center_element} >
+                    <div className="font-medium" onClick={() => disconnectUser()}>
+                        log out
+                    </div>
+                </div>}
             </div>
         </div>
     )
--- pages/_app.js
+++ pages/_app.js
@@ -27,31 +27,33 @@ function MyApp({ Component, pageProps }) {
 
   const iubendaScript = `
   var _iub = _iub || [];
-  _iub.csConfiguration = {
-    "askConsentAtCookiePolicyUpdate":true,
-    "enableFadp":true,
-    "enableLgpd":true,
-    "enableUspr":true,
-    "fadpApplies":true,
-    "lang":"en",
-    "perPurposeConsent":true,
-    "siteId":3354885,
-    "usprApplies":true,
-    "whitelabel":false,
-    "cookiePolicyId":94474485, 
-    "banner":{ 
-      "acceptButtonColor":"#D60B52",
-      "acceptButtonDisplay":true,
-      "backgroundColor":"#1B1B1D",
-      "closeButtonDisplay":false,
-      "customizeButtonColor":"#303030",
-      "customizeButtonDisplay":true,
-      "explicitWithdrawal":true,
-      "listPurposes":true,
-      "logo":null,
-      "position":"float-bottom-right",
-      "rejectButtonColor":"#D60B52",
-      "rejectButtonDisplay":true }};
+_iub.csConfiguration = {
+  "askConsentAtCookiePolicyUpdate":true,
+  "countryDetection":true,
+  "enableFadp":true,
+  "enableLgpd":true,
+  "enableUspr":true,
+  "lang":"en",
+  "lgpdAppliesGlobally":false,
+  "perPurposeConsent":true,
+  "siteId":3354885,
+  "whitelabel":false,
+  "cookiePolicyId":94474485, 
+  "banner":{ 
+    "acceptButtonColor":"#D60B52",
+    "acceptButtonDisplay":true,
+    "backgroundColor":"#1B1B1D",
+    "closeButtonDisplay":false,
+    "customizeButtonColor":"#303030",
+    "customizeButtonDisplay":true,
+    "explicitWithdrawal":true,
+    "listPurposes":true,
+    "logo":null,
+    "position":"float-bottom-right",
+    "rejectButtonColor":"#D60B52",
+    "rejectButtonDisplay":true,
+    "showPurposesToggles":true 
+  }};;
   `;
 
   return (
