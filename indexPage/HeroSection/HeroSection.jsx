7e481169cbdf7c94929640b5a4a197023af4d2cd
Pelepo <andretta.filippo.99@gmail.com>
2024-04-04 09:00:48 +0200

    New title

--- indexPage/HeroSection/HeroSection.jsx
+++ indexPage/HeroSection/HeroSection.jsx
@@ -12,9 +12,8 @@ const HeroSection = () => {
             <div className={Style.heroSection_box}>
                 <div className={Style.heroSection_box_colums}>
                     <div className={Style.heroSection_box_colums_title}>
-                        <h1 className="font-huge">REVIVING <span className='bold'>MUSIC COLLECTING</span><br />
-                            BY <span className='bold'>DIGITIZING</span> ITS PRACTICES <br />
-                            THROUGH BLOCKCHAIN</h1>
+                        <h1 className="font-huge">COLLECT <span className='bold'>UNRELEASED</span><br />
+                            <span className='bold'>LIMITED EDITIONS</span> TRACKS</h1>
                     </div>
                     <ExternalLinkButton text="learn more" path="https://www.docs.lirmusic.com" background="white" textColor="black" />
 
