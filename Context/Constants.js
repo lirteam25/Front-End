b4f8c2b8c8013b5809af6da3b63c989bf510a76f
Pelepo <andretta.filippo.99@gmail.com>
2024-05-03 17:00:26 +0200

    Right contract address

--- Context/Constants.js
+++ Context/Constants.js
@@ -3,15 +3,15 @@ import oldNftMarketplace from "./NFTMarketplaceUpgradableV2.json";
 
 const NFTMarketplaceAddressNormCase = "0xBa7FB0f905dFC69bD144C6Cc2E763D28f62eFc4e";
 const NFTMarketplaceAddressNormCase_DEV = "0x79b046BaEaBCeea366365B617E0086225F1d9873";
-export const NFTMarketplaceAddress = process.env.NODE_ENV == "polygon" ? NFTMarketplaceAddressNormCase : NFTMarketplaceAddressNormCase_DEV;
+export const NFTMarketplaceAddress = process.env.ACTIVE_CHAIN == "polygon" ? NFTMarketplaceAddressNormCase : NFTMarketplaceAddressNormCase_DEV;
 
 const NFTMarketplaceContractId = null;
 const NFTMarketplaceContractId_DEV = "8e78cdd7-e70f-44cb-b46b-17f1c839243f"
-export const NFTMarketplaceAddressContractId = process.env.NODE_ENV == "polygon" ? NFTMarketplaceContractId : NFTMarketplaceContractId_DEV;
+export const NFTMarketplaceAddressContractId = process.env.ACTIVE_CHAIN == "polygon" ? NFTMarketplaceContractId : NFTMarketplaceContractId_DEV;
 
 const OldMarketplaceContractAddress = "0xBa7FB0f905dFC69bD144C6Cc2E763D28f62eFc4e";
 const OldMarketplaceContractAddress_DEV = "0x014e12408911830d31a7846593f937b66b2caa46";
-export const OldNFTMarketplaceAddress = process.env.NODE_ENV == "polygon" ? OldMarketplaceContractAddress : OldMarketplaceContractAddress_DEV;
+export const OldNFTMarketplaceAddress = process.env.ACTIVE_CHAIN == "polygon" ? OldMarketplaceContractAddress : OldMarketplaceContractAddress_DEV;
 export const OldNFTMarketplaceABI = oldNftMarketplace.abi
 
 //Marketplace Owner
