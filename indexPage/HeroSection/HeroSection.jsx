425469acb154ac10c8e7d858e14d2e4d4dc8dac8
Pelepo <andretta.filippo.99@gmail.com>
2024-05-07 16:29:18 +0200

    No prova page, no linear gradient, notion on mob

--- indexPage/HeroSection/HeroSection.jsx
+++ indexPage/HeroSection/HeroSection.jsx
@@ -16,10 +16,8 @@ const HeroSection = () => {
                             <span className='bold'>LIMITED EDITIONS</span> TRACKS</h1>
                     </div>
                     <ExternalLinkButton text="learn more" path="https://lirmusic.notion.site/Lir-Music-info-694b4a6252224f9fba741bc2397f6212?pvs=4" background="white" textColor="black" />
-
                 </div>
             </div>
-            <div className={Style.heroSection_gradient} />
         </div>
     );
 };
