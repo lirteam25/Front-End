ad07f0ea182c1a8148ce71ea10195d4fb63a868e
Pelepo <andretta.filippo.99@gmail.com>
2024-05-24 18:57:08 +0200

    Not on staging

--- NFTDetailsPage/NFTDetailsImg/BuyItem/BuyItem.jsx
+++ NFTDetailsPage/NFTDetailsImg/BuyItem/BuyItem.jsx
@@ -56,7 +56,7 @@ const BuyItem = ({ nft, setOpenBuy }) => {
                         collectionId={nft.collection_id}
                         projectId={process.env.CROSSMINT_PROJECT_ID}
                         mintConfig={{ "totalPrice": nft.price.toString(), "quantity": "1", "tokenId": nft.token_id.toString() }}
-                        environment="staging"
+                        environment={process.env.ACTIVE_CHAIN == "polygon" ? "production " : "staging"}
                         checkoutProps={{ "paymentMethods": ["fiat", "ETH", "SOL"] }}
                         mintTo={address}
                         onEvent={(event) => {
