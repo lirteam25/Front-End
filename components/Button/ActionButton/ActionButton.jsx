7c318f70e0269bf4c5e68ea772137970f25d44f3
Pelepo <andretta.filippo.99@gmail.com>
2024-06-26 20:56:19 +0200

    from btn to div

--- components/Button/ActionButton/ActionButton.jsx
+++ components/Button/ActionButton/ActionButton.jsx
@@ -11,9 +11,9 @@ const ActionButton = ({ action, text, fontSize }) => {
 
     return (
         <div className={Style.ActionButton} style={buttonStyle} >
-            <button onClick={async () => action()} className={Style.ActionButton_text}>
+            <div onClick={async () => action()} className={Style.ActionButton_text}>
                 {text}
-            </button>
+            </div>
         </div>
     )
 }
