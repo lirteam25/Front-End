f8f6344230cee974bd06fba2b9e4fc3faf2ba72e
Pelepo <andretta.filippo.99@gmail.com>
2024-07-16 18:41:36 +0200

    Mobile version

--- pages/collection.js
+++ pages/collection.js
@@ -39,6 +39,22 @@ const Discover = () => {
                         Electronic
                     </div>
                 </div>
+                <div className={Style.mobile_discover_filter}>
+                    <div className={`${Style.mobile_discover_filter_element} ${!filter && Style.active} font-small`} onClick={() => { if (filter) { setFilter(false) } }}>
+                        All Genres
+                    </div>
+                    <div className={Style.mobile_discover_filter_bottom}>
+                        <div className={`${Style.discover_filter_element} ${filter == "Techno" && Style.active} font-small`} onClick={() => { if (filter != "Techno") { setFilter("Techno") } }}>
+                            Techno
+                        </div>
+                        <div className={`${Style.discover_filter_element} ${filter == "House" && Style.active} font-small`} onClick={() => { if (filter != "House") { setFilter("House") } }}>
+                            House
+                        </div>
+                        <div className={`${Style.discover_filter_element} ${filter == "Electronic" && Style.active} font-small`} onClick={() => { if (filter != "Electronic") { setFilter("Electronic") } }}>
+                            Electronic
+                        </div>
+                    </div>
+                </div>
                 <div className={Style.discover_items}>
                     <NFTCardTwo sellingNFTs={filter ? filteredNFts : data} />
                 </div>
