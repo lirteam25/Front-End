b064ce95dbce8360e59701708d5cec5f6b9f4628
Pelepo <andretta.filippo.99@gmail.com>
2024-07-17 11:47:25 +0200

    Straight input tables

--- indexPage/HeroSection/HeroSection.jsx
+++ indexPage/HeroSection/HeroSection.jsx
@@ -49,28 +49,22 @@ const HeroSection = () => {
 
     return (
         <div className={Style.heroSection}>
-            <div>
-                <div className={`${Style.heroSection_banner} font-normal`}>
-                    <span className='bold'>free lifetime release</span> for the first <span className='bold'>20</span> artists
-                </div>
-                <div className={Style.heroSection_colums}>
-                    <div className={Style.heroSection_colums_text}>
-                        <h1 className='font-huge'>
-                            <br />
-                            YOUR <span id="animatedWord" className={`${Style.heroSection_colums_text_animateWord} ${rolling ? Style.fadeOut : ''} bold`}>{words[index]}</span>  <br />
-                        </h1>
-                        <p className='font-normal'>
-                            Create your <span className='bold'>digital vinyl</span>.
-                            <br />Set the <span className='bold'>supply</span>, <span className='bold'>price</span>, and upload your <span className='bold'>unreleased track</span>.
-                            <br />No production costs or shippings. Let fans <span className='bold'>collect</span> and <span className='bold'>resell digitally</span>.
-                        </p>
-                        {emailSent ?
-                            <div className={Style.heroSection_colums_text_button}>
-                                <div className={Style.heroSection_colums_text_button_active}>
-                                    <InfoButton text="Email correctly sent" />
-                                </div>
-                            </div> :
-                            <div className={Style.heroSection_colums_text_button}>
+            <div className={Style.heroSection_colums}>
+                <div className={Style.heroSection_colums_text}>
+                    <h1 className='font-huge bold'>
+                        ENHANCING THE VALUE OF DIGITAL MUSIC.
+                    </h1>
+                    {emailSent ?
+                        <div className={Style.heroSection_colums_text_button}>
+                            <div className={Style.heroSection_colums_text_button_active}>
+                                <InfoButton text="Email correctly sent" />
+                            </div>
+                        </div> :
+                        <div className={Style.heroSection_colums_text_button}>
+                            <h2 className={`${Style.heroSection_colums_text_button_explanation} font-large`}>
+                                Release the first song for free ↓
+                            </h2>
+                            <div className={Style.heroSection_colums_text_button_flex}>
                                 <input
                                     type="string"
                                     placeholder="Insert Your Email"
@@ -83,11 +77,11 @@ const HeroSection = () => {
                                     }
                                 </div>
                             </div>
-                        }
-                    </div>
-                    <div className={Style.heroSection_colums_floppy}>
-                        <Image src={img.landing_floppy} alt='floppy disk' className={Style.heroSection_colums_floppy_img} />
-                    </div>
+                        </div>
+                    }
+                </div>
+                <div className={Style.heroSection_colums_floppy}>
+                    <Image src={img.landing_floppy} alt='floppy disk' className={Style.heroSection_colums_floppy_img} />
                 </div>
             </div>
         </div>
