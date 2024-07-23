634881d8156f858038a2d28c3486a90b6fcc87bc
Pelepo <andretta.filippo.99@gmail.com>
2024-07-23 12:25:27 +0200

    Sing => SIGN

--- indexPage/JoinLir/JoinLir.jsx
+++ indexPage/JoinLir/JoinLir.jsx
@@ -31,7 +31,7 @@ const JoinLir = () => {
                 <div>
                     <span className='font-large'>Start Collecting</span>
                     <div className={Style.JoinLir_buttons_button}>
-                        <ButtonConnectWallet text="SING IN" />
+                        <ButtonConnectWallet text="SIGN IN" />
                     </div>
                 </div>
                 <div>
